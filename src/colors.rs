b3a19b1ce5604d6ba20d67f6c601a77ecccbc663
Nbiba Bedis <bedisnbiba@gmail.com>
2021-02-18 18:55:46 +0100

    add colors

---
+++ src/colors.rs
@@ -0,0 +1,63 @@
+#![allow(missing_docs)]
+const DEL1: &str = "\x1b[";
+const DEL2: &str = "m";
+const DEL3: &str = "\x1b[0m";
+const FG: &str = "38";
+
+fn color(string: &str, color: Palette) -> String {
+    //"\x1b[$(fg/bg);2;$r;$g;$bm$string\x1b[0m"
+    format!("{}{};2;{}{}{}{}", DEL1, FG, color, DEL2, string, DEL3)
+}
+
+enum Palette {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    LightBlue,
+    RGB(u8, u8, u8),
+}
+
+impl std::fmt::Display for Palette {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Palette::*;
+        match self {
+            Red => write!(f, "255;0;0"),
+            Yellow => write!(f, "255;255;0"),
+            Green => write!(f, "0;255;0"),
+            Blue => write!(f, "0;0;255"),
+            LightBlue => write!(f, "0;150;255"),
+            RGB(r, g, b) => write!(f, "{};{};{}", r, g, b),
+        }
+    }
+}
+
+pub trait Color {
+    fn red(&self) -> String;
+    fn green(&self) -> String;
+    fn yellow(&self) -> String;
+    fn blue(&self) -> String;
+    fn light_blue(&self) -> String;
+    fn rgb(&self, r: u8, g: u8, b: u8) -> String;
+}
+impl Color for &str {
+    fn red(&self) -> String {
+        color(&self, Palette::Red)
+    }
+    fn green(&self) -> String {
+        color(&self, Palette::Green)
+    }
+    fn yellow(&self) -> String {
+        color(&self, Palette::Yellow)
+    }
+    fn blue(&self) -> String {
+        color(&self, Palette::Blue)
+    }
+    fn light_blue(&self) -> String {
+        color(&self, Palette::LightBlue)
+    }
+    fn rgb(&self, r: u8, g: u8, b: u8) -> String {
+        color(&self, Palette::RGB(r, g, b))
+    }
+}
+
